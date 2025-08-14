0064009099d3685e3cfddbc1fedb403a4da5aa01
jiehuikoo <koojiehui@outlook.com>
2025-08-14 16:19:28 +0800

    feat(wiki): dataframe generator for content of countries

--- rag_and_flag_ai/wiki.py
+++ rag_and_flag_ai/wiki.py
@@ -4,6 +4,7 @@
 # %% Import wikipedia API
 import wikipedia
 # %% Import other libraries
+import pandas as pd
 
 # %% Read the list of countries from a text file
 def get_list_of_countries(file_path):
@@ -24,12 +25,26 @@ def get_list_of_countries(file_path):
     return countries
 
 # %% Fetch content for each country from Wikipedia
-def get_country_content(country):
+def get_country_content(query):
     """
     Fetches the content of a Wikipedia page for a given country.
     """
+    max_results = 10  # Maximum number of results to return
+    keywords = ["country"]
+
+    wikipedia.set_lang("en")  # Set the language to English
+    
+
     try:
-        page = wikipedia.page(country)
+        results = wikipedia.search(query, results=max_results)
+        print(results)
+        filtered = [r for r in results if any(k.lower() in r.lower() for k in keywords)]
+        best_match = filtered[0] if filtered else results[0]
+        print(f"✅ Best match: {best_match}")
+
+        # Fetch the page
+        page = wikipedia.page(best_match ,auto_suggest=False)
+
         return page.content
     except wikipedia.exceptions.PageError:
         print(f"Page not found for {country}")
@@ -41,14 +56,17 @@ def get_country_content(country):
 # %%
 
 list_of_countries = get_list_of_countries("list_of_countries.txt")
-
-# %%
-
-for country in list_of_countries:
-    content = get_country_content(country)
-    if content:
-        print(f"Content for {country}:\n{content[:500]}...\n")  # Print first 500 characters
-    else:
-        print(f"No content found for {country}\n")
-
     
+# %% Fetch content for each country
+country_content = {}
+for country in list_of_countries:
+    print(f"Fetching content for {country}...")
+    content = get_country_content(country)
+    country_content[country] = content
+
+# %% The following countries have special cases
+country_content["Libyan Arab Jamahiriya"] = get_country_content("Libya")
+country_content["Macedonia"] = get_country_content("Macedonia (region)")
+country_content["Congo"] = get_country_content("Democratic Republic of the Congo")
+# %% Generate the dataframe
+country_content_df = pd.DataFrame(country_content.items(), columns=["Country", "Content"])
